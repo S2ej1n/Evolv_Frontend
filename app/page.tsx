ff310bc7a4a6e471d6c5fb90213f0a56a7b3a1f1
sejin <ks320205@naver.com>
2026-01-13 18:53:43 +0900

    design: 공용 레이아웃 컴포넌트 헤더 제작

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
+// Main 고객 리스트 페이지
 
-export default function Home() {
+export default function Customers() {
   return (
     <div className="p-10">
-      {/* 배경색(bg-blue-500), 둥근 모서리(rounded), 그림자(shadow)가 잘 보이나요? */}
       <div className="p-6 bg-blue-500 text-white font-bold rounded-xl shadow-lg">
-        Tailwind CSS가 잘 작동하고 있습니다! 🚀
+        Tailwind CSS가 잘 작동하고 있습니다! 
       </div>
     </div>
   );
