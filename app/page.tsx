2d25c3ee12c44cbaf733eadd7d348550444895ca
sejin <ks320205@naver.com>
2026-01-14 10:04:07 +0900

    design: shadcn/ui 페이지네이션 기본 ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 // Main 고객 리스트 페이지
-
 import ContentBox from "@/components/ContentBox";
-
+import Pagination from "@/components/Pagination";
 
 export default function Customers() {
   return (
     <main>
       <ContentBox>
         <h1 className="text-lg font-semibold">고객 리스트</h1>
+        <Pagination/>
       </ContentBox>
     </main>
   );
--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,1 +1,29 @@
 // [공통 UI 컴포넌트] 페이지네이션 (제작)
+'use client';
+
+import { 
+    Pagination as MAINPagination, 
+    PaginationContent, PaginationItem, 
+    PaginationLink, PaginationNext, PaginationPrevious 
+} from "./ui/pagination";
+
+export default function Pagination() {
+  return (
+    <MAINPagination>
+        <PaginationContent>
+            <PaginationItem>
+                <PaginationPrevious />
+            </PaginationItem>
+
+            <PaginationItem>
+                <PaginationLink isActive>1</PaginationLink>
+            </PaginationItem>
+
+
+            <PaginationItem>
+                <PaginationNext />
+            </PaginationItem>
+        </PaginationContent>
+    </MAINPagination>
+  )
+}
